8b9c5feb5b20802792e2a098340d8f8026ed7e27
Alex Doss <alexdoss@Alexs-MacBook-Pro-5.local>
2019-05-02 15:16:03 -0600

    Adds Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import './App.css';
+import store from './redux/store'
+import {HashRouter} from 'react-router-dom'
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    
+    <Provider store = {store}>
+      <HashRouter>
+        <Navbar />
+
+      </HashRouter>
+    </Provider>
   );
 }
 
